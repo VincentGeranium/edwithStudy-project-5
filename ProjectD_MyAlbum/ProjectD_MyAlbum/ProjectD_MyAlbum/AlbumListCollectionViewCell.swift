ebbd6dbdf715df5eb83821b210757703f0c2d3dd
김광준 <kwangjun3952@gmail.com>
2020-01-12 21:42:02 +0900

    feat: make main view
    first. make AlbumListViewController
    second. make albumListCollectionView
    third. make albumListCollectionViewCell
    fourth. import init AppIcon
    
    make basic UI frame about UICollectionView and UICollectionViewCell

---
+++ ProjectD_MyAlbum/ProjectD_MyAlbum/ProjectD_MyAlbum/AlbumListCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  AlbumListCollectionViewCell.swift
+//  ProjectD_MyAlbum
+//
+//  Created by 김광준 on 2020/01/12.
+//  Copyright © 2020 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class AlbumListCollectionViewCell: UICollectionViewCell {
+    
+    static let cellId = "albumListCollectionViewCell"
+    
+//    var thumbnailImage
+    
+    private let thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ ProjectD_MyAlbum/ProjectD_MyAlbum/ProjectD_MyAlbum/ImageManager.swift
@@ -0,0 +1,13 @@
+//
+//  ImageManager.swift
+//  ProjectD_MyAlbum
+//
+//  Created by 김광준 on 2020/01/12.
+//  Copyright © 2020 VincentGeranium. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Photos
+
+let imageManager: PHCachingImageManager = PHCachingImageManager()
