724f161ed28aa5dd121bb159728a8dc1fd92b8c3
김광준 <kwangjun3952@gmail.com>
2020-02-03 17:06:44 +0900

    200203 진행중

--- ProjectD_MyAlbum/ProjectD_MyAlbum/ProjectD_MyAlbum/AlbumListViewController.swift
+++ ProjectD_MyAlbum/ProjectD_MyAlbum/ProjectD_MyAlbum/AlbumListViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 import Photos
 
 class AlbumListViewController: UIViewController {
-
-    var albumAssetCollectionArray: [PHAssetCollection] = []
     
-    var albumAssetID: [String] = []
+    private var fetchResult: PHFetchResult<PHAsset>?
+    private var fetchResultOfCollection: PHFetchResult<PHAssetCollection>?
+    
+    let fetchSortDescriptorKey: String = "creationDate"
+    
+    let albumListTitle = "앨범"
+    
+    var albumResult: PHAssetCollection?
+    
+    var albumsCount: Int?
     
     private let albumListCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -26,14 +33,7 @@ class AlbumListViewController: UIViewController {
         collectionView.register(AlbumListCollectionViewCell.self, forCellWithReuseIdentifier: AlbumListCollectionViewCell.cellId)
         return collectionView
     }()
-    
-    private var fetchResult: PHFetchResult<PHAsset>?
-    private var thumbnailAsset: PHAsset?
-    private var fetchResultOfCollection: PHFetchResult<PHAssetCollection>?
-    private var assetFetchResultss: PHFetchResult<AnyObject>?
-    
-    let fetchSortDescriptorKey: String = "creationDate"
-    let albumListTitle = "앨범"
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,34 +63,89 @@ class AlbumListViewController: UIViewController {
         ])
     }
     
+    var fetchResultOfCollectionArray: [PHAssetCollection] = []
+    var phAssetArray: [PHAsset] = []
+    var fetchResultOfAssetArray: [PHFetchResult<PHAsset>] = []
 
-    var fetchResultArray: [PHFetchResult<PHAssetCollection>?] = []
-    var id: [String] = []
-    var id2: [String] = []
-    var assetCollectionArray: [PHAssetCollection] = []
-    var albumTitles: [String] = []
-    var albumConunt: [Int] = []
-    var albumFirstObjects: [PHAsset] = []
-
-    
     private func requestCollection() {
-        let fetchOptions = PHFetchOptions()
-        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-        let getMyAlbums: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: fetchOptions)
         
- 
-        getMyAlbums.enumerateObjects { (collection, index, stop) in
-            print(collection.estimatedAssetCount)
+        
+        getAlbumData()
+    
+    }
+    
+    private func getAlbumData() {
+        let option = PHFetchOptions()
+        let getAllAlbums: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: option)
+    
+        
+        var getPhotoCountOfAllAlbums: PHFetchResult<PHAsset>?
+        
+        // 각 앨범 별 이름 배열
+        var eachAlbumsTitles: [String] = []
+        
+        // PHAssetCollection 배열
+        var getAllAlbumsArray: [PHAssetCollection] = []
+        
+        // 각 앨범 별 사진 갯수 배열
+        var getAllPhotosCount: [Int] = []
+        
+        var eachAlbum: PHAssetCollection?
+        
+        for i in 0 ..< getAllAlbums.count {
+            // 각각의 앨범
+            eachAlbum = getAllAlbums[i]
             
-            let fetchResults = PHAsset.fetchAssets(in: collection, options: fetchOptions)
+            guard let eachAlbums = eachAlbum else {
+                return
+            }
             
-            print(fetchResults)
+            albumResult = getAllAlbumsArray[i]
             
+            // 각각의 앨범 명 저장
+            eachAlbumsTitles.append(eachAlbums.localizedTitle!)
+            // 각각의 앨범 안에 있는 사진 수를 가져오기 위해 배열에 저장
+            getAllAlbumsArray.append(getAllAlbums[i])
+            // 각각의 앨범 안에 있는 사진 수를 알아오기 위해 PHAsset.fetchAssets를 사용, 배열에 저장해 둔 각각의 앨범 PHAssetCollection 사용
+            getPhotoCountOfAllAlbums = PHAsset.fetchAssets(in: getAllAlbumsArray[i], options: nil)
+            // 각각의 앨범의 사진 수 배열에 저장
+            if let photoCount = getPhotoCountOfAllAlbums {
+                getAllPhotosCount.append(photoCount.count)
+            }
         }
         
+        print(eachAlbumsTitles.count)
+        
+        print("♥️\(getAllPhotosCount.count)")
+        
+        // 셀 갯수 생성을 위한 모든 앨범의 이름 갯수를 넣어줌
+        albumsCount = eachAlbumsTitles.count
         
         
-
+    }
+    
+    private func getThumbnail(albumResult: PHAssetCollection, targetSize: CGSize, cell: AlbumListCollectionViewCell) {
+//        DispatchQueue.async(execute: .global(qos: DispatchQoS.background))
+//        DispatchQueue.global(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0).async(execute: <#() -> Void#>)
+        DispatchQueue.global(qos: DispatchQoS.QoSClass.background).async {
+            let fetchOption = PHFetchOptions()
+            fetchOption.predicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.image.rawValue)
+            
+            let manager = imageManagerDefault
+            
+            let assetFetchResult = PHAsset.fetchAssets(in: albumResult, options: fetchOption)
+            
+            if assetFetchResult.count > 0 {
+                if let imageAsset = assetFetchResult.lastObject as? PHAsset {
+                    let requestOption = PHImageRequestOptions()
+                    requestOption.isSynchronous = false
+                    requestOption.deliveryMode = .highQualityFormat
+                    manager.requestImage(for: imageAsset, targetSize: targetSize, contentMode: .aspectFill, options: requestOption) { image, _ in
+                        cell.thumbnailImage = image
+                    }
+                }
+            }
+        }
     }
     
 
@@ -139,48 +194,54 @@ extension AlbumListViewController: UICollectionViewDelegateFlowLayout, UICollect
 
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return self.fetchResultOfCollection?.count ?? 0
-//        return self.fetchResult?.count ?? 0
-        return self.assetFetchResultss?.count ?? 0
+
+        return self.albumsCount ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell: AlbumListCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: AlbumListCollectionViewCell.cellId, for: indexPath) as? AlbumListCollectionViewCell else {
             return UICollectionViewCell()
         }
-        
-        
-       
-        let asset: PHAsset = fetchResult?.object(at: indexPath.item) ?? PHAsset()
-        
-        var assets: PHAsset = (assetFetchResultss?.object(at: indexPath.item)) as! PHAsset
-        
         let widthAndHeight: CGFloat = (UIScreen.main.bounds.width / 2) - 15
         
-   
+        let targerSize = CGSize(width: widthAndHeight, height: widthAndHeight)
+        
+        if let albumResult = albumResult {
+            getThumbnail(albumResult: albumResult, targetSize: targerSize, cell: cell)
+        }
+ 
+//        getThumbnail(albumResult: albumResult, targetSize: targerSize, cell: cell)
+        
+//        let widthAndHeight: CGFloat = (UIScreen.main.bounds.width / 2) - 15
+//
+//        var asset: PHAsset?
+//
+//        for i in 0 ..< fetchResultOfAssetArray.count {
+//
+//            asset = fetchResultOfAssetArray[i].object(at: indexPath.item)
+//
+//
+//        }
+//
+//
+//            cachingImageManager.requestImage(for: asset!,
+//                                      targetSize: CGSize(width: widthAndHeight, height: widthAndHeight),
+//                                      contentMode: .aspectFill,
+//                                      options: nil) { image, _ in
+//                                        cell.thumbnailImage  = image
+//            }
+//
+//
+//        print("🔵 \(phAssetArray[0])")
+//        print("🔵 \(phAssetArray[1])")
+//        print(phAssetArray.count)
+//
         
 
-    
-        imageManager.requestImage(for: assets,
-                                              targetSize: CGSize(width: widthAndHeight, height: widthAndHeight),
-                                              contentMode: .aspectFill,
-                                              options: nil) {image, _ in
-                                                cell.thumbnailImage = image
-                    }
-        
-
-        
-      
-
-
-        
-//        getThumbnail(cell: cell)
-        
-        
         return cell
     }
     
-    
+
     
     
 }
